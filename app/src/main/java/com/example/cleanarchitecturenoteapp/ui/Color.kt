fdfe73389850ea0f771329eb12d7d9049a3b3d0c
shakhzod_bakhromov <shakhzod_bakhromov@mail.ru>
2023-12-05 11:36:12 +0000

    Initial commit

---
+++ app/src/main/java/com/example/cleanarchitecturenoteapp/ui/Color.kt
@@ -0,0 +1,8 @@
+package com.example.cleanarchitecturenoteapp.ui
+
+import android.graphics.Color
+
+/*
+val DarkGray = Color(0xFF202020)
+val LightBlue = Color(0xFFD7E8DE)
+val fromLongBlue = Color(0xFF0000FF)*/
