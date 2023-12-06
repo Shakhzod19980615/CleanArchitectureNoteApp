a6b990c97b6b929aa09a92424d13d6844ab8a9d2
shakhzod_bakhromov <shakhzod_bakhromov@mail.ru>
2023-12-06 13:03:35 +0000

    built roomDB

--- app/src/main/java/com/example/cleanarchitecturenoteapp/ui/Color.kt
+++ app/src/main/java/com/example/cleanarchitecturenoteapp/ui/Color.kt
@@ -1,8 +1,11 @@
 package com.example.cleanarchitecturenoteapp.ui
 
-import android.graphics.Color
+import androidx.compose.ui.graphics.Color
 
-/*
 val DarkGray = Color(0xFF202020)
 val LightBlue = Color(0xFFD7E8DE)
-val fromLongBlue = Color(0xFF0000FF)*/
+val RedOrange = Color(0xFFFFab91)
+val RedPink = Color(0xFFF48Fb1)
+val BabyBlue = Color(0xFF81DEEA)
+val Violet = Color(0xFFCF94DA)
+val LightGreen = Color(0xFFE7ed9b)
