666d333a1f327a968ccc22bc7f05cbf90bc74ca4
shakhzod_bakhromov <shakhzod_bakhromov@mail.ru>
2023-12-07 06:52:46 +0000

    built repository

--- app/src/main/java/com/example/cleanarchitecturenoteapp/feature_note/domain/use_case/GetNotes.kt
+++ app/src/main/java/com/example/cleanarchitecturenoteapp/feature_note/domain/use_case/GetNotes.kt
@@ -2,13 +2,18 @@ package com.example.cleanarchitecturenoteapp.feature_note.domain.use_case
 
 import com.example.cleanarchitecturenoteapp.feature_note.domain.model.Note
 import com.example.cleanarchitecturenoteapp.feature_note.domain.repository.NoteRepository
+import com.example.cleanarchitecturenoteapp.feature_note.domain.util.NoteOrder
+import com.example.cleanarchitecturenoteapp.feature_note.domain.util.OrderType
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
 
 class GetNotes(
-    private  val noteRepository: NoteRepository
+    private  val repository: NoteRepository
 ) {
 
-    operator fun invoke() : Flow<List<Note>>{
-
+    operator fun invoke(
+        noteOrder: NoteOrder = NoteOrder.Date(OrderType.Descending)
+    ) : Flow<List<Note>>{
+        return repository.getNotes().map { notes ->}
     }
 }
